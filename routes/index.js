e91816ee28bcf88a775248b94b3f692724bced3f
Ravi Kallepalli <ravi@austinwebdeveloper.co>
2019-08-07 11:06:32 +0000

    backend

--- routes/index.js
+++ routes/index.js
@@ -1,109 +1,103 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI('abe83e20f730477984459fbf65e2bc82');
-
-
+const NewsAPI = require("newsapi");
+const newsapi = new NewsAPI("abe83e20f730477984459fbf65e2bc82");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-
-router.get('/home', function(req, res, next) 
-{    
-  newsapi.v2.topHeadlines({
-    category: 'general',
-    language: 'en',
-    country: 'in'
-  }).then(response => {
-    console.log(response);
-  res.send(response);
-  });
-   
+router.get("/home", function(req, res, next) {
+  newsapi.v2
+    .topHeadlines({
+      category: "general",
+      language: "en",
+      country: "in"
+    })
+    .then(response => {
+      console.log(response);
+      res.send(response);
+    });
 });
 
-router.get('/business', function(req, res, next) 
-{    
-  newsapi.v2.topHeadlines({
-    category: 'business',
-    language: 'en',
-    country: 'in'
-  }).then(response => {
-    console.log(response);
-  res.send(response);
-  });
-   
+router.get("/business", function(req, res, next) {
+  newsapi.v2
+    .topHeadlines({
+      category: "business",
+      language: "en",
+      country: "in"
+    })
+    .then(response => {
+      console.log(response);
+      res.send(response);
+    });
 });
 
-router.get('/sports', function(req, res, next) 
-{    
-  newsapi.v2.topHeadlines({
-    category: 'sports',
-    language: 'en',
-    country: 'in'
-  }).then(response => {
-    console.log(response);
-  res.send(response);
-  });
-   
+router.get("/sports", function(req, res, next) {
+  newsapi.v2
+    .topHeadlines({
+      category: "sports",
+      language: "en",
+      country: "in"
+    })
+    .then(response => {
+      console.log(response);
+      res.send(response);
+    });
 });
 
-router.get('/entertainment', function(req, res, next) 
-{    
-  newsapi.v2.topHeadlines({
-    category: 'entertainment',
-    language: 'en',
-    country: 'in'
-  }).then(response => {
-    console.log(response);
-  res.send(response);
-  });
-   
+router.get("/entertainment", function(req, res, next) {
+  newsapi.v2
+    .topHeadlines({
+      category: "entertainment",
+      language: "en",
+      country: "in"
+    })
+    .then(response => {
+      console.log(response);
+      res.send(response);
+    });
 });
 
-
-
-router.get('/science', function(req, res, next) 
-{    
-  newsapi.v2.topHeadlines({
-    category: 'science',
-    language: 'en',
-    country: 'in'
-  }).then(response => {
-    console.log(response);
-  res.send(response);
-  });
-   
+router.get("/science", function(req, res, next) {
+  newsapi.v2
+    .topHeadlines({
+      category: "science",
+      language: "en",
+      country: "in"
+    })
+    .then(response => {
+      console.log(response);
+      res.send(response);
+    });
 });
 
-router.get('/technology', function(req, res, next) 
-{    
-  newsapi.v2.topHeadlines({
-    category: 'tetchnology',
-    language: 'en',
-    country: 'in'
-  }).then(response => {
-    console.log(response);
-  res.send(response);
-  });
-   
+router.get("/technology", function(req, res, next) {
+  newsapi.v2
+    .topHeadlines({
+      category: "technology",
+      language: "en",
+      country: "in"
+    })
+    .then(response => {
+      console.log(response);
+      res.send(response);
+    });
 });
 
-router.get('/health', function(req, res, next) 
-{    
-  newsapi.v2.topHeadlines({
-    category: 'health',
-    language: 'en',
-    country: 'in'
-  }).then(response => {
-    console.log(response);
-  res.send(response);
-  });
-   
+router.get("/health", function(req, res, next) {
+  newsapi.v2
+    .topHeadlines({
+      category: "health",
+      language: "en",
+      country: "in"
+    })
+    .then(response => {
+      console.log(response);
+      res.send(response);
+    });
 });
-
 
 router.get("/inter-business", (req, res) => {
   newsapi.v2
@@ -128,7 +122,6 @@ router.get("/inter-sports", (req, res) => {
       res.send(response);
     });
 });
-
 
 router.get("/inter-entertainment", (req, res) => {
   newsapi.v2
@@ -157,7 +150,7 @@ router.get("/inter-science", (req, res) => {
 router.get("/inter-technology", (req, res) => {
   newsapi.v2
     .topHeadlines({
-      category: "tetchnology",
+      category: "technology",
       language: "en"
     })
     .then(response => {
@@ -171,7 +164,6 @@ router.get("/inter-health", (req, res) => {
     .topHeadlines({
       category: "health",
       language: "en"
-     
     })
     .then(response => {
       console.log(response);
@@ -179,19 +171,17 @@ router.get("/inter-health", (req, res) => {
     });
 });
 
-router.post('/search', function(req, res, next) 
-{    
-  newsapi.v2.topHeadlines({
-     q: req.body.searchValue ,
-      country:"in",
-    language: 'en',
-  }).then(response => {
-    console.log(response);
-  res.send(response);
-  });
-   
+router.post("/search", function(req, res, next) {
+  newsapi.v2
+    .topHeadlines({
+      q: req.body.searchValue,
+      country: "in",
+      language: "en"
+    })
+    .then(response => {
+      console.log(response);
+      res.send(response);
+    });
 });
-
-
 
 module.exports = router;
